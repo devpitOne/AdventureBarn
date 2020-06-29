4a2e8b76bb76055cb4ce1c1e401a807352a9f4ea
DevpitOne <hexpector@gmail.com>
2020-06-29 18:37:11 +0100

    Content changes to views, product and address unified partial views Unfinished Stock Graph, paused for now

---
+++ AdventureBarn.WorkSite/Scripts/Graph.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+	var c = document.getElementById("StockGraph");
+	var ctx = c.getContext("2d");
+	var grd = ctx.createLinearGradient(0, 0, 300, 0);
+	grd.addColorStop(0, "blue");
+	grd.addColorStop(1, "black");
+	ctx.fillStyle = grd;
+	ctx.fillRect(20, Math.random() * 200, 20, 200);
+	ctx.fillRect(80, Math.random() * 200, 20, 200);
+	ctx.fillRect(140, Math.random() * 200, 20, 200);
+	ctx.fillRect(200, Math.random() * 200, 20, 200);
+});
